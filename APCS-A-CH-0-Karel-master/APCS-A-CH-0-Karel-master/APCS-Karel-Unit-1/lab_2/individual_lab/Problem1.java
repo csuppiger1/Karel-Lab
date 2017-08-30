1ada3dc64fb1bbf70554fac6f4bad513a911f7ff
csuppiger1 <31353414+csuppiger1@users.noreply.github.com>
2017-08-30 09:58:28 -0700

    Lab 2 Day 1

--- APCS-A-CH-0-Karel-master/APCS-A-CH-0-Karel-master/APCS-Karel-Unit-1/lab_2/individual_lab/Problem1.java
+++ APCS-A-CH-0-Karel-master/APCS-A-CH-0-Karel-master/APCS-Karel-Unit-1/lab_2/individual_lab/Problem1.java
@@ -15,6 +15,72 @@ public class Problem1 extends Robot
     }
 
     public void setPins(){
-        
+        positionLeftSide();
+        firstRow();
+        repositionRight();
+        secondRow();
+        repositionLeft();
+        thirdRow();
+        repositionRight();
+        fourthRow();
+    } public void turnRight(){
+       turnLeft();
+       turnLeft();
+       turnLeft();
+    } public void move2() {
+        move();
+        move();
+    }
+    /**
+       * moves the robot to the left bottom corner
+       */
+    public void positionLeftSide() {
+        turnLeft();
+        move();
+        move();
+        move();
+        move();
+        turnRight();
+        turnRight();
+    } public void firstRow() {
+        move2();
+        move2();
+        putBeeper();
+        move2();
+        move2();
+    } public void repositionRight() {
+        turnLeft();
+        move();
+        turnLeft();
+    } public void secondRow() {
+        move2();
+        move();
+        putBeeper();
+        move2();
+        putBeeper();
+        move();
+        move2();
+    } public void repositionLeft() {
+        turnRight();
+        move();
+        turnRight();
+    } public void thirdRow() {
+        move2();
+        putBeeper();
+        move2();
+        putBeeper();
+        move2();
+        putBeeper();
+        move2();
+    } public void fourthRow() {
+        move();
+        putBeeper();
+        move2();
+        putBeeper();
+        move2();
+        putBeeper();
+        move2();
+        putBeeper();
+        move();      
     }
 }
--- APCS-A-CH-0-Karel-master/APCS-A-CH-0-Karel-master/APCS-Karel-Unit-1/lab_2/individual_lab/Problem2.java
+++ APCS-A-CH-0-Karel-master/APCS-A-CH-0-Karel-master/APCS-Karel-Unit-1/lab_2/individual_lab/Problem2.java
@@ -3,7 +3,7 @@ package lab_2.individual_lab;
 import kareltherobot.*;
 import java.awt.Color;
 
-public class Problem2 extends Robot
+public class Problem2 extends Problem1
 {
    
     /**
@@ -15,6 +15,13 @@ public class Problem2 extends Robot
     }
 
     public void setField(){
+        setPins();
+        repositionLeft();
+        thirdRow();
+        repositionRight();
+        secondRow();
+        repositionLeft();
+        firstRow();
         
     }
 }
